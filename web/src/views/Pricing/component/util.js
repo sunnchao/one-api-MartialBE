81be338304cfaaee4e42bf0130c67f2f87d1faf4
sunchaowang <sunchaowang@outlook.com>
2024-04-26 09:11:12 +0800

    fixed 优化模型界面小数点

--- web/src/views/Pricing/component/util.js
+++ web/src/views/Pricing/component/util.js
@@ -7,5 +7,5 @@ export function ValueFormatter(value) {
   if (value == null) {
     return '';
   }
-  return `$${parseFloat(value * 0.002).toFixed(4)} / ￥${parseFloat(value * 0.014).toFixed(4)}`;
+  return `$${parseFloat(value * 0.002).toFixed(5)} / ￥${parseFloat(value * 0.014).toFixed(5)}`;
 }
