212e8a20eebcfd7aa71986e7dd56e2d2d4ebc6c6
sunchaowang <sunchaowang@outlook.com>
2024-05-15 14:58:56 +0800

    优化 tokens 的计算

--- common/token.go
+++ common/token.go
@@ -6,10 +6,9 @@ import (
 	"math"
 	"strings"
 
+	"github.com/MartialBE/tiktoken-go"
 	"one-api/common/image"
 	"one-api/types"
-
-	"github.com/MartialBE/tiktoken-go"
 )
 
 var tokenEncoderMap = map[string]*tiktoken.Tiktoken{}
