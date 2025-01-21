afe6d10d3c5fb997b94f7e1d52a2b05b95e316e5
sunchaowang <sunchaowang@outlook.com>
2025-01-21 09:34:31 +0800

    ✨ feat: 添加签到和登录日志类型

--- web/src/views/Log/type/LogType.js
+++ web/src/views/Log/type/LogType.js
@@ -3,7 +3,9 @@ const LOG_TYPE = {
   1: { value: '1', text: '充值', color: 'primary' },
   2: { value: '2', text: '消费', color: 'orange' },
   3: { value: '3', text: '管理', color: 'default' },
-  4: { value: '4', text: '系统', color: 'secondary' }
+  4: { value: '4', text: '系统', color: 'secondary' },
+  5: { value: '5', text: '签到', color: 'primary' },
+  6: { value: '6', text: '登录', color: 'primary' }
 };
 
 export default LOG_TYPE;
